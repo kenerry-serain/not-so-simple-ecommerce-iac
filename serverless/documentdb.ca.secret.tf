ee9b71bb9778f27b94562b01c757301391c39139
Kenerry Serain <kserain.development@gmail.com>
2024-05-16 06:52:20 -0300

    [Files] Removing unnecessary

--- serverless/documentdb.ca.secret.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_s3_object" "documentdb_certificate" {
-  bucket = aws_s3_bucket.nsse.bucket
-  key    = "app/documentdb-ca.pem"
-  source = "${path.module}/lambdas/report-job/documentdb-ca.pem"
-  etag = filemd5("${path.module}/lambdas/report-job/documentdb-ca.pem")
-}
